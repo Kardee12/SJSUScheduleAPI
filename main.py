5f1115b57ea6c7789ceafbde6f4c38259bc97037
Kardee12 <emailclub72@gmail.com>
2023-07-11 19:56:23 -0700

    Added Genericness bullshit

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
 from typing import List
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Query
+from fastapi.responses import JSONResponse
 import scraper
 
 app = FastAPI()
 
+data_scraper = scraper.ScrapeData()
+json_data = data_scraper.to_json(data_scraper.scrapeData())
+
 @app.get("/")
 async def root():
-    data_scraper = scraper.scrapeData()
-    json_data = data_scraper.to_json()
-    return json_data
+    return JSONResponse(content=json_data)
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+
+@app.get("/courses/")
+async def listCourses(skip: int = 0, limit: int = 10):
+    return json_data[skip: skip + limit]
+
+
+@app.get("/courses/multiple")
+async def getMultipleDepartmentsCourses(d: List[str] = Query(...)):
+    print(d)
+    lowercase_departments = [dept.lower() for dept in d]
+    filtered_courses = []
+    for course in json_data:
+        for dept in lowercase_departments:
+            if dept == course.get("department", "").lower():
+                filtered_courses.append(course)
+                break
+    return JSONResponse(content=filtered_courses)
+
+
+@app.get("/courses/professor")
+async def getCoursesByProfessor(professor: str):
+    filtered_courses = []
+
+    for course in json_data:
+        if professor.lower() in course["instructor"].lower():
+            filtered_courses.append(course)
+
+    return JSONResponse(content = filtered_courses)
+
+
+@app.get("/courses/search")
+async def searchCourses(
+        department: str = Query(None),
+        professor: str = Query(None),
+        course: str = Query(None),
+        units: float = Query(None),
+        mode_of_instruction: str = Query(None)
+):
+    filtered_courses = []
+
+    for courseEntry in json_data:
+        if department and courseEntry["department"].lower() != department.lower():
+            continue
+        if professor and professor.lower() not in courseEntry["instructor"].lower():
+            continue
+        if course and course.lower() not in courseEntry["course"].lower():
+            continue
+        if units and courseEntry["units"] != units:
+            continue
+        if mode_of_instruction and mode_of_instruction.lower() not in courseEntry["mode_of_instruction"].lower():
+            continue
+        filtered_courses.append(courseEntry)
+
+    return JSONResponse(content = filtered_courses)
+
--- scraper.py
+++ scraper.py
@@ -1,22 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
-
-import json
-import requests
-from bs4 import BeautifulSoup
 import re
+from pydantic import BaseModel
 
-class scrapeData():
+
+class CourseScheduleEntry(BaseModel):
+    department: str
+    course: str
+    section: str
+    class_number: str
+    mode_of_instruction: str
+    course_title: str
+    satisfies: str
+    units: int
+    class_type: str
+    days: str
+    times: str
+    instructor: str
+    location: str
+    dates: str
+    open_seats: int
+    notes: str
+
+
+class ScrapeData():
+
+    def to_json(self, data):
+        return [entry.dict() for entry in data]
+
     def scrapeData(self):
         # Make a GET request to the webpage containing the class schedule table
         url = "https://www.sjsu.edu/classes/schedules/fall-2023.php"
         response = requests.get(url)
 
-        # Parse the HTML content using BeautifulSoup
         soup = BeautifulSoup(response.content, "html.parser")
-
-        # Find the class schedule table
-        table = soup.find("table", id="classSchedule")
+        table = soup.find("table", id = "classSchedule")
         class_schedule = []
 
         for row in table.find_all("tr"):
@@ -30,45 +48,39 @@ class scrapeData():
             mode_of_instruction = columns[2].text.strip()
             course_title = columns[3].text.strip()
             satisfies = columns[4].text.strip()
-            units = columns[5].text.strip()
+            units = int(float(columns[5].text.strip()))
             class_type = columns[6].text.strip()
             days = columns[7].text.strip()
             times = columns[8].text.strip()
             instructor = columns[9].text.strip()
             location = columns[10].text.strip()
             dates = columns[11].text.strip()
-            open_seats = columns[12].text.strip()
+            open_seats = int(float(columns[12].text.strip()))
             notes = columns[13].text.strip()
 
-            entry = {
-                "department": department,
-                "course": course,
-                "section": section,
-                "class_number": class_number,
-                "mode_of_instruction": mode_of_instruction,
-                "course_title": course_title,
-                "satisfies": satisfies,
-                "units": units,
-                "class_type": class_type,
-                "days": days,
-                "times": times,
-                "instructor": instructor,
-                "location": location,
-                "dates": dates,
-                "open_seats": open_seats,
-                "notes": notes
-            }
-
+            entry = CourseScheduleEntry(
+                department = department,
+                course = course,
+                section = section,
+                class_number = class_number,
+                mode_of_instruction = mode_of_instruction,
+                course_title = course_title,
+                satisfies = satisfies,
+                units = units,
+                class_type = class_type,
+                days = days,
+                times = times,
+                instructor = instructor,
+                location = location,
+                dates = dates,
+                open_seats = open_seats,
+                notes = notes
+            )
             class_schedule.append(entry)
-
+        print("Scraping Class Schedule...")
         return class_schedule
 
-    def to_json(self):
-        data = self.scrapeData()
-        return data
-
-# # Usage example
-# data_scraper = scrapeData()
-# json_data = data_scraper.to_json()
+# Usage example
+# data_scraper = ScrapeData()
+# json_data = data_scraper.to_json(data_scraper.scrapeData())
 # print(json_data)
-
